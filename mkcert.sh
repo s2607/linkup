920db80e08e443cfccb89870a75da0631188a772
Linux User <swiley@thinkpad.my.domain>
2018-04-06 19:41:44 +0000

    fixed mkcert (again)

--- mkcert.sh
+++ mkcert.sh
@@ -4,4 +4,4 @@ echo "its not my fault if thats hard for you"
 
 openssl genrsa -out server.key 2048
 openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000
+openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000  -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com"
