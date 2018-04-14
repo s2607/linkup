bcddbab6ceb5a6525c325a10eba21c45c7ecda09
Tyler Cypher <tcyph33@gmail.com>
2018-04-14 12:20:12 -0400

    updated mkcert.sh

--- mkcert.sh
+++ mkcert.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+#^
 echo "if you get an error go install openssl"
 echo "its not my fault if thats hard for you"
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
 
 openssl genrsa -out server.key 2048
 openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000  -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com"
+case "${machine}" in
+	Linux) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000  -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com";;
+	Mac) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000  -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com";;
+	MinGw) openssl req -new -x509 -sha256 -key server.key -out server.crt -days 9000  -subj "//C=US\ST=CA\O=Acme, Inc.\CN=example.com";;
+	*) echo "no."; exit 1
+esac
